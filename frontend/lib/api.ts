2ad68cb8df0d4ad014f0bbb45a1252941e1680c4
Wursteintopf <markus@vogelvlug.de>
2022-04-15 13:28:03 +0200

    Use public env variable in frontend

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -7,7 +7,7 @@ function getInternalStrapiURL(path = "") {
 
 function getExternalStrapiURL(path = "") {
   return `${
-    process.env.STRAPI_API_URL || "http://localhost:1337"
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
   }${path}`;
 }
 
